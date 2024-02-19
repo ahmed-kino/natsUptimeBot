e7e05e38c1ceff50e600ebb5f8b056dbccc5a683
Ahmed Kino <ehmedaff@gmail.com>
2024-02-19 15:15:18 +0000

    change secret key to include in the env for now

--- natsUptimeBot/settings/base.py
+++ natsUptimeBot/settings/base.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 
-with open(os.path.join(BASE_DIR, "secret_key.txt")) as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Application definition
 
